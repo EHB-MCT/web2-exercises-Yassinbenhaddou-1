4e1071d065b09b7889d1dc59a453a2c27959dd1d
Yassinbenhaddou <43605983+Yassinbenhaddou@users.noreply.github.com>
2021-10-14 11:40:55 +0200

    S3 Ex2: step1 & 2: add html and basic fetch url

--- S3 EX1/script.js
+++ S3 EX1/script.js
@@ -7,24 +7,51 @@ window.onload = () => {
         
         
         let numberResultField = document.getElementById("number").value;
-        numberResultField = parseInt(numberResultField)
+        numberResultField = parseInt(numberResultField);
         console.log(random);
-        console.log(numberResultField);
 
-        if(numberResultField < random)
-        {
-            console.log("The mystery number is lower. Guess again!");
-        }
-        else if(numberResultField > random)
-        {
-            console.log("The mystery number is higher. Guess again!");
-        }
-        else
-        {
-            console.log("You have guessed the mystery number!");
-        }
-
+        compareNumber(numberResultField).then(
+            result => addMessage(result, '5'),
+            error => addMessage(error, 'E')
+        );
        
     });
 
+}
+
+function compareNumber(numberResultField) {
+    if ( numberResultField < 1 || numberResultField > 20)
+    {
+        document.getElementById("errorMessage")
+        .innerHTML = "That is not a valid number (Error) ";
+
+        document.getElementById("message").innerHTML = "";
+
+    }else
+    {
+        if(numberResultField > random)
+        {
+            document.getElementById("message")
+            .innerHTML = "The mystery number is higher. Guess again!";
+
+            document.getElementById("errorMessage").innerHTML = "";
+
+        }
+        else if(numberResultField < random)
+        {
+            document.getElementById("message")
+            .innerHTML = "The mystery number is lower. Guess again!";
+
+            document.getElementById("errorMessage").innerHTML = "";
+
+        }
+        else
+        {
+            document.getElementById("message")
+            .innerHTML = "You have guessed the mystery number!";
+            
+            document.getElementById("errorMessage").innerHTML = "";
+
+        }
+    }
 }
---
+++ S3 Ex2/script.js
@@ -0,0 +1,18 @@
+window.onload = () => 
+{
+
+    
+
+    //step 1 & step 2
+    /*
+    fetch("http://www.omdbapi.com/?i=tt6751668&apikey=cb08d5c5")
+    .then(response => response.json())
+    .then(data => console.log(data));
+    */
+
+    let apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=cb08d5c5";
+    let apiKey = "&apikey=cb08d5c5";
+
+
+
+}
